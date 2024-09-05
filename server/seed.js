e586fd665cb5e1162371be1da93a1f5bc7aa4cdb
Jonathan Taylor <jon.1807@hotmail.co.uk>
2024-09-05 13:06:53 +0100

    added seed

--- server/seed.js
+++ server/seed.js
@@ -10,3 +10,4 @@ async function seed() {
   const query = `INSERT INTO messages (name, message, likes) VALUES($1, $2, $3)`;
   const result = await db.query(query, ["Luigi", "Lets a go", 2]);
 }
+seed();
